629f4e55aeecedfc882e422eb509d92fd822a6c8
Richard Hülsberg <richard.huelsberg@twt.de>
2022-02-26 09:25:40 +0100

    implement macroable trait composer allow php8.1 implement php8.1

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,14 @@ ENV DEBIAN_FRONTEND noninteractive
 
 RUN export DEBIAN_FRONTEND=${DEBIAN_FRONTEND}
 RUN apt-get -qy update && apt-get -qy upgrade && apt-get -qy install software-properties-common unzip wget
-RUN add-apt-repository ppa:ondrej/php && apt-get -qy update
-RUN apt-get -qy install php8.0-cli php8.0-readline php8.0-mysql php8.0-mbstring php8.0-redis php8.0-amqp php8.0-xml \
-    php8.0-intl php8.0-zip php8.0-xhprof php8.0-xdebug php8.0-opcache php8.0-mcrypt php8.0-curl php8.0-gd \
-    php8.0-imagick
+RUN add-apt-repository ppa:ondrej/php
+RUN apt-get -qy update
+RUN apt-get -qy install \
+    php8.0-cli php8.0-readline php8.0-mysql php8.0-mbstring php8.0-redis php8.0-amqp php8.0-xml php8.0-intl \
+    php8.0-zip php8.0-xhprof php8.0-xdebug php8.0-opcache php8.0-mcrypt php8.0-curl php8.0-gd php8.0-imagick
+RUN apt-get -qy install \
+    php8.1-cli php8.1-readline php8.1-mysql php8.1-mbstring php8.1-redis php8.1-amqp php8.1-xml php8.1-intl \
+    php8.1-zip php8.1-xhprof php8.1-xdebug php8.1-opcache php8.1-mcrypt php8.1-curl php8.1-gd php8.1-imagick
 
 RUN echo 'xdebug.mode=debug' >> /etc/php/8.0/cli/php.ini
 RUN echo 'xdebug.client_host=host.docker.internal' >> /etc/php/8.0/cli/php.ini
@@ -22,10 +26,12 @@ RUN mkdir -p /app && chown tinyframework:tinyframework /app
 RUN echo "PS1='bash$ '" >> /etc/bash.bashrc
 RUN echo "PATH=\"\$PATH:/opt/sonar-scanner/bin\"" >> /etc/bash.bashrc
 RUN echo "export PHP_IDE_CONFIG=serverName=tinyframework" >> /etc/bash.bashrc
+RUN echo "alias phpx8.0=\"php8.0 -dxdebug.mode=debug -dxdebug.client_host=host.docker.internal -dxdebug.client_port=9003 -dxdebug.start_with_request=yes \$@\"" >> /etc/bash.bashrc
+RUN echo "alias phpx8.1=\"php8.1 -dxdebug.mode=debug -dxdebug.client_host=host.docker.internal -dxdebug.client_port=9003 -dxdebug.start_with_request=yes \$@\"" >> /etc/bash.bashrc
 RUN echo "alias phpx=\"php -dxdebug.mode=debug -dxdebug.client_host=host.docker.internal -dxdebug.client_port=9003 -dxdebug.start_with_request=yes \$@\"" >> /etc/bash.bashrc
 
 RUN mkdir -p /opt/sonar-scanner
-RUN wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip --output-document=/tmp/sonar-scanner-cli-4.6.2.2472-linux.zip
+RUN wget -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.2.2472-linux.zip --output-document=/tmp/sonar-scanner-cli-4.6.2.2472-linux.zip
 RUN unzip -d /tmp/ /tmp/sonar-scanner-cli-4.6.2.2472-linux.zip
 RUN mv /tmp/sonar-scanner*/* /opt/sonar-scanner
 RUN rm -rf /tmp/sonar-scanner*
---
+++ src/Helpers/Macroable.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TinyFramework\Helpers;
+
+use Closure;
+use ReflectionClass;
+use BadMethodCallException;
+use InvalidArgumentException;
+
+trait Macroable
+{
+
+    /** @var Closure[]|callable[] */
+    private static array $staticMacros = [];
+
+    /** @var Closure[]|callable[] */
+    private static array $macros = [];
+
+    public static function addStaticMacro(string $name, Closure|callable $callback): void
+    {
+        $reflectionClass = new ReflectionClass(static::class);
+        if ($reflectionClass->hasMethod($name)) {
+            $reflectionMethod = $reflectionClass->getMethod($name);
+            if ($reflectionMethod->isStatic()) {
+                throw new InvalidArgumentException(
+                    sprintf(
+                        'Could not overwrite real static method %s::%s()!',
+                        static::class,
+                        $name
+                    )
+                );
+            }
+        }
+        static::$staticMacros[$name] = $callback;
+    }
+
+    public static function addMacro(string $name, Closure|callable $callback): void
+    {
+        $reflectionClass = new ReflectionClass(static::class);
+        if ($reflectionClass->hasMethod($name)) {
+            $reflectionMethod = $reflectionClass->getMethod($name);
+            if (!$reflectionMethod->isStatic()) {
+                throw new InvalidArgumentException(
+                    sprintf(
+                        'Could not overwrite real method %s::%s()!',
+                        static::class,
+                        $name
+                    )
+                );
+            }
+        }
+        static::$macros[$name] = $callback;
+    }
+
+    public static function hasStaticMacro(string $name): bool
+    {
+        return array_key_exists($name, static::$staticMacros);
+    }
+
+    public static function hasMacro(string $name): bool
+    {
+        return array_key_exists($name, static::$macros);
+    }
+
+    public static function __callStatic(string $name, array $arguments): mixed
+    {
+        if (static::hasStaticMacro($name)) {
+            $callback = static::$staticMacros[$name];
+            if ($callback instanceof Closure) {
+                $callback->bindTo(null, static::class);
+            }
+            return $callback(...$arguments);
+        }
+        throw new BadMethodCallException(
+            sprintf(
+                'Real Method or Macromethod %s::%s() does not exists.',
+                static::class,
+                $name
+            )
+        );
+    }
+
+    public function __call(string $name, array $arguments): mixed
+    {
+        if (static::hasMacro($name)) {
+            $callback = static::$macros[$name];
+            if ($callback instanceof Closure) {
+                return $callback->bindTo($this, static::class);
+            }
+            return $callback(...$arguments);
+        }
+        throw new BadMethodCallException(
+            sprintf(
+                'Real Method or Macromethod %s->%s() does not exists.',
+                static::class,
+                $name
+            )
+        );
+    }
+
+}
