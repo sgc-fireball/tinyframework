5da3d1a3f58b9cd8bc3f8f654e9f9ac9bc7a225a
Richard Hülsberg <richard.huelsberg@twt.de>
2022-05-26 21:52:27 +0200

    implement a cronjob --list

--- src/Console/Commands/TinyframeworkCronjobCommand.php
+++ src/Console/Commands/TinyframeworkCronjobCommand.php
@@ -6,9 +6,11 @@ namespace TinyFramework\Console\Commands;
 
 use DateTimeImmutable;
 use DateTimeZone;
+use phpDocumentor\Reflection\Types\Integer;
 use TinyFramework\Console\CommandAwesome;
 use TinyFramework\Console\Input\InputDefinitionInterface;
 use TinyFramework\Console\Input\InputInterface;
+use TinyFramework\Console\Input\Option;
 use TinyFramework\Console\Output\OutputInterface;
 use TinyFramework\Cron\CronExpression;
 use TinyFramework\Cron\CronjobInterface;
@@ -19,13 +21,21 @@ class TinyframeworkCronjobCommand extends CommandAwesome
     protected function configure(): InputDefinitionInterface
     {
         return parent::configure()
-            ->description('Cronjob Service');
+            ->description('Cronjob Service')
+            ->option(Option::create('list', 'l', Option::VALUE_OPTIONAL, 'Prints a list with all cronjobs.', false));
     }
 
     public function run(InputInterface $input, OutputInterface $output): int
     {
         parent::run($input, $output);
+        if ($input->option('list')) {
+            return $this->runCronjobList($input, $output);
+        }
+        return $this->runCronjobs($input, $output);
+    }
 
+    private function runCronjobs(InputInterface $input, OutputInterface $output): int
+    {
         $timezone = new DateTimeZone($this->container->get('config')->get('app.timezone', 'UTC'));
         $now = new DateTimeImmutable('now', $timezone);
         /** @var LoggerInterface $logger */
@@ -39,11 +49,13 @@ class TinyframeworkCronjobCommand extends CommandAwesome
                 /** @var int $verbosity */
                 $verbosity = $this->output->verbosity();
                 if (!$mustRun && $verbosity >= OutputInterface::VERBOSITY_VERBOSE) {
-                    $this->output->writeln(sprintf(
-                        "\r[<gold>SKIP</gold>] <yellow>%s</yellow> next: %s",
-                        get_class($job),
-                        $cron->getNextRunDate()->format('Y-m-d H:i:s')
-                    ));
+                    $this->output->writeln(
+                        sprintf(
+                            "\r[<gold>SKIP</gold>] <yellow>%s</yellow> next: %s",
+                            get_class($job),
+                            $cron->getNextRunDate()->format('Y-m-d H:i:s')
+                        )
+                    );
                 }
                 return $mustRun;
             }
@@ -54,21 +66,41 @@ class TinyframeworkCronjobCommand extends CommandAwesome
             try {
                 $this->output->write(sprintf("\r[....] <yellow>%s</yellow>", get_class($job)));
                 $job->handle();
-                $this->output->write(sprintf(
-                    "\r[<green>DONE</green>] <yellow>%s</yellow> in %.2f secs.\n",
-                    get_class($job),
-                    microtime(true) - $start
-                ));
+                $this->output->write(
+                    sprintf(
+                        "\r[<green>DONE</green>] <yellow>%s</yellow> in %.2f secs.\n",
+                        get_class($job),
+                        microtime(true) - $start
+                    )
+                );
             } catch (\Throwable $e) {
-                $this->output->write(sprintf(
-                    "\r[<red>FAIL</red>] <yellow>%s</yellow> in %.2f secs.\n",
-                    get_class($job),
-                    microtime(true) - $start
-                ));
+                $this->output->write(
+                    sprintf(
+                        "\r[<red>FAIL</red>] <yellow>%s</yellow> in %.2f secs.\n",
+                        get_class($job),
+                        microtime(true) - $start
+                    )
+                );
                 $logger->error(exception2text($e, true));
             }
         }
-
         return 0;
     }
+
+    private function runCronjobList(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('# For information see the manual pages of crontab(5) and cron(8)');
+        $output->writeln('#');
+        $output->writeln('# m h  dom mon dow   command');
+        $output->writeln('');
+        foreach ($this->container->tagged('cronjob') as $job) {
+            assert($job instanceof CronjobInterface);
+            $expression = new CronExpression($job->expression());
+            $output->writeln($job->expression() . ' \\' . get_class($job));
+            $output->writeln('# next run: ' . $expression->getNextRunDate()->format('Y-m-d H:i:s'));
+            $output->writeln('');
+        }
+        return 0;
+    }
+
 }
