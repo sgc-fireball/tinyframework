836a7790762edb40349e3e5c578996acdefea9a6
Richard Hülsberg <richard.huelsberg@twt.de>
2023-02-21 23:14:30 +0100

    implement gps

---
+++ src/Helpers/GPS.php
@@ -0,0 +1,184 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TinyFramework\Helpers;
+
+/**
+ * @see https://www.koordinaten-umrechner.de/decimal/51.388923,6.642949
+ */
+class GPS
+{
+    protected string $latitudeDirections = 'N';
+
+    protected int $latitudeHours = 0;
+
+    protected int $latitudeMinutes = 0;
+
+    protected float $latitudeSeconds = 0;
+
+    protected string $longitudeDirection = 'E';
+
+    protected int $longitudeHours = 0;
+
+    protected int $longitudeMinutes = 0;
+
+    protected float $longitudeSeconds = 0;
+
+    public function __construct(string $position)
+    {
+        $position = preg_replace("/[^NSOEW0-9\-\+\.\"\'\s]/", '', $position);
+        if (preg_match('/^([\-\+]{0,1}\d{1,2})\.(\d{1,}) ([\-\+]{0,1}\d{1,3})\.(\d{1,})$/', $position, $match)) {
+            $this->latitudeDirections = ($match[1] < 0) ? 'S' : 'N';
+            $this->latitudeHours = (int)$match[1];
+            $this->latitudeHours = $this->latitudeHours < 0 ? $this->latitudeHours * -1 : $this->latitudeHours;
+            $tmp = (float)('0.' . $match[2]) * 60;
+            $this->latitudeMinutes = (int)$tmp;
+            $this->latitudeSeconds = (float)($tmp - (int)$tmp) * 60;
+            $this->longitudeDirection = ($match[3] < 0) ? 'W' : 'E';
+            $this->longitudeHours = (int)$match[3];
+            $this->longitudeHours = $this->longitudeHours < 0 ? $this->longitudeHours * -1 : $this->longitudeHours;
+            $tmp = (float)('0.' . $match[4]) * 60;
+            $this->longitudeMinutes = (int)$tmp;
+            $this->longitudeSeconds = (float)($tmp - (int)$tmp) * 60;
+            // dd mm.mmm
+        } elseif (preg_match(
+            '/^(N|S) (\d{1,2}) (\d{1,2})\.(\d{1,6}) (O|E|W) (\d{1,3}) (\d{1,2})\.(\d{1,6})$/i',
+            $position,
+            $match
+        )) {
+            if ($match[5] == 'O') {
+                $match[5] = 'E';
+            }
+            $this->latitudeDirections = strtoupper($match[1]);
+            $this->latitudeHours = (int)$match[2];
+            $this->latitudeMinutes = (int)$match[3];
+            $this->latitudeSeconds = (float)($match[4] / 1000000 * 60);
+            $this->longitudeDirection = strtoupper($match[5]);
+            $this->longitudeHours = (int)$match[6];
+            $this->longitudeMinutes = (int)$match[7];
+            $this->longitudeSeconds = (float)($match[8] / 1000000 * 60);
+        } elseif (preg_match(
+            '/^(N|S)\s{0,1}(\d{1,2}) (\d{1,2}) (\d{1,2}) (E|O|W)\s{0,1}(\d{1,2}) (\d{1,2}) (\d{1,2})$/',
+            $position,
+            $match
+        )) {
+            if ($match[5] == 'O') {
+                $match[5] = 'E';
+            }
+            $this->latitudeDirections = strtoupper($match[1]);
+            $this->latitudeHours = (int)$match[2];
+            $this->latitudeMinutes = (int)$match[3];
+            $this->latitudeSeconds = (int)$match[4];
+            $this->longitudeDirection = strtoupper($match[5]);
+            $this->longitudeHours = (int)$match[6];
+            $this->longitudeMinutes = (int)$match[7];
+            $this->longitudeSeconds = (int)$match[8];
+        } elseif (preg_match(
+            '/^(N|S) (\d{1,2}) (\d{1,2})\' (\d{1,2}.\d{1,4})["|\'\'] (E|O|W) (\d{1,3}) (\d{1,2})\' (\d{1,2}.\d{1,4})["|\'\']$/i',
+            $position,
+            $match
+        )) {
+            if ($match[6] == 'O') {
+                $match[6] = 'E';
+            }
+            $this->latitudeDirections = strtoupper($match[1]);
+            $this->latitudeHours = (int)$match[2];
+            $this->latitudeMinutes = (int)$match[3];
+            $this->latitudeSeconds = (float)$match[4];
+            $this->longitudeDirection = strtoupper($match[5]);
+            $this->longitudeHours = (int)$match[6];
+            $this->longitudeMinutes = (int)$match[7];
+            $this->longitudeSeconds = (float)$match[8];
+        }
+    }
+
+    public function getGPS(): string
+    {
+        return sprintf(
+            '%s %s° %s.%s %s %s° %s.%s',
+            $this->latitudeDirections,
+            $this->latitudeHours,
+            $this->latitudeMinutes,
+            round($this->latitudeSeconds / 60 * 1000000),
+            $this->longitudeDirection,
+            $this->longitudeHours,
+            $this->longitudeMinutes,
+            round($this->longitudeSeconds / 60 * 1000000)
+        );
+    }
+
+    public function getLatitude(): float
+    {
+        $result = $this->latitudeDirections == 'N' ? '' : '-';
+        $result .= round($this->latitudeHours + ($this->latitudeMinutes / 60) + (($this->latitudeSeconds) / 3600), 6);
+        return (float)$result;
+    }
+
+    public function getLongitude(): float
+    {
+        $result = $this->longitudeDirection == 'W' ? '-' : '';
+        $result .= round($this->longitudeHours + ($this->longitudeMinutes / 60) + (($this->longitudeSeconds) / 3600), 6);
+        return (float)$result;
+    }
+
+    public function getWGS84Degree(): string
+    {
+        return sprintf(
+            '%s %s %s %s',
+            $this->latitudeDirections,
+            number_format($this->latitudeHours + $this->latitudeMinutes / 60 + $this->latitudeSeconds / 3600, 6),
+            $this->longitudeDirection,
+            number_format($this->longitudeHours + $this->longitudeMinutes / 60 + $this->longitudeSeconds / 3600, 6)
+        );
+    }
+
+    public function getWGS84Seconds(): string
+    {
+        // dd° mm' ss.s"
+        return sprintf(
+            '%s %s° %s\' %s" %s %s° %s\' %s"',
+            $this->latitudeDirections,
+            $this->latitudeHours,
+            $this->latitudeMinutes,
+            number_format($this->latitudeSeconds, 3),
+            $this->longitudeDirection,
+            $this->longitudeHours,
+            $this->longitudeMinutes,
+            number_format($this->longitudeSeconds, 3)
+        );
+    }
+
+    public function distance(GPS|string $position, string $unit = 'km'): float
+    {
+        $position = $position instanceof self ? $position : new self($position);
+        $latitudeA = $this->getLatitude();
+        $longitudeA = $this->getLongitude();
+        $latitudeB = $position->getLatitude();
+        $longitudeB = $position->getLongitude();
+
+        if (($latitudeA == $latitudeB) && ($longitudeA == $longitudeB)) {
+            return 0;
+        }
+
+        $latFrom = deg2rad($latitudeA);
+        $lonFrom = deg2rad($longitudeA);
+        $latTo = deg2rad($latitudeB);
+        $lonTo = deg2rad($longitudeB);
+        $latDelta = $latTo - $latFrom;
+        $lonDelta = $lonTo - $lonFrom;
+        $angle = 2 * asin(sqrt((sin($latDelta / 2) ** 2) + cos($latFrom) * cos($latTo) * (sin($lonDelta / 2) ** 2)));
+        $km = $angle * 6371000;
+
+        $unit = strtolower($unit);
+        if (in_array($unit, ['k', 'km', 'kilometer'])) {
+            return $km;
+        } elseif (in_array($unit, ['m', 'miles'])) {
+            return $km * 1.609344;
+        } elseif (in_array($unit, ['n', 'nm', 'nautical miles'])) {
+            return $km * 18.52;
+        } else {
+            throw new \InvalidArgumentException('Unknown distance type: ' . $unit);
+        }
+    }
+}
---
+++ tests/Helpers/GPSTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TinyFramework\Tests\Helpers;
+
+use PHPUnit\Framework\TestCase;
+use TinyFramework\Helpers\GPS;
+
+/**
+ * @link https://coordinates-converter.com/en/decimal/51.389930,6.638880?karte=OpenStreetMap&zoom=8
+ * Decimal degree: N 51.389933 E 6.638888
+ * Degrees Minutes: N 51° 23.395800 E 6° 38.332800
+ * Degrees Minutes Seconds: N 51° 23' 23.748 E 6° 38' 19.968
+ */
+class GPSTest extends TestCase
+{
+
+    public function testOne()
+    {
+        $gps = new GPS('51.389933 6.638883');
+        $this->assertEquals(51.389933, $gps->getLatitude());
+        $this->assertEquals(6.638883, $gps->getLongitude());
+        $this->assertEquals('N 51° 23.395980 E 6° 38.332980', $gps->getGPS());
+        $this->assertEquals('N 51.389933 E 6.638883', $gps->getWGS84Degree());
+        $this->assertEquals('N 51° 23\' 23.759" E 6° 38\' 19.979"', $gps->getWGS84Seconds());
+    }
+
+    public function testTwo()
+    {
+        $gps = new GPS('N 51° 23.395800 E 6° 38.332800');
+        $this->assertEquals(51.38993, $gps->getLatitude());
+        $this->assertEquals(6.63888, $gps->getLongitude());
+        $this->assertEquals('N 51° 23.395800 E 6° 38.332800', $gps->getGPS());
+        $this->assertEquals('N 51.389930 E 6.638880', $gps->getWGS84Degree());
+        $this->assertEquals('N 51° 23\' 23.748" E 6° 38\' 19.968"', $gps->getWGS84Seconds());
+    }
+
+    public function testThree()
+    {
+        $gps = new GPS('N 51° 23\' 23.759" E 6° 38\' 19.979"');
+        $this->assertEquals(51.389933, $gps->getLatitude());
+        $this->assertEquals(6.638883, $gps->getLongitude());
+        $this->assertEquals('N 51° 23.395983 E 6° 38.332983', $gps->getGPS());
+        $this->assertEquals('N 51.389933 E 6.638883', $gps->getWGS84Degree());
+        $this->assertEquals('N 51° 23\' 23.759" E 6° 38\' 19.979"', $gps->getWGS84Seconds());
+    }
+
+}
