e67e787565d43502f122c9dc070ce36739a46ebd
Richard Hülsberg <richard.huelsberg@twt.de>
2023-05-01 20:59:04 +0200

    logging long running tests

---
+++ tests/LongrunningTestAlert.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TinyFramework\Tests;
+
+use PHPUnit\Runner\AfterTestHook;
+
+/**
+ * @internal
+ */
+class LongRunningTestAlert implements AfterTestHook
+{
+    protected const MAX_SECONDS_ALLOWED = 1;
+
+    public function executeAfterTest(string $test, float $time): void
+    {
+        if ($time > self::MAX_SECONDS_ALLOWED) {
+            fwrite(STDERR, sprintf("\nThe %s test took %s seconds!\n", $test, $time));
+        }
+    }
+}
