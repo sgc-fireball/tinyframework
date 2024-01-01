d9ed63b3326129899d574aefa7efc39d080db0c4
Richard Hülsberg <richard.huelsberg@twt.de>
2024-01-01 12:55:06 +0100

    improve http test case

--- src/PHPUnit/HttpTestCase.php
+++ src/PHPUnit/HttpTestCase.php
@@ -7,6 +7,7 @@ namespace TinyFramework\PHPUnit;
 use PHPUnit\Framework\TestCase;
 use TinyFramework\Core\Container;
 use TinyFramework\Core\TestHttpKernel;
+use TinyFramework\Session\SessionInterface;
 
 abstract class HttpTestCase extends TestCase
 {
@@ -14,17 +15,21 @@ abstract class HttpTestCase extends TestCase
 
     protected ?TestHttpKernel $kernel;
 
+    protected ?SessionInterface $session;
+
     protected function setUp(): void
     {
         parent::setUp();
         $this->container = Container::instance();
         $this->kernel = $this->container->get(TestHttpKernel::class);
+        $this->session = $this->container->get('session')->clear();
     }
 
     protected function tearDown(): void
     {
         unset($this->container);
         unset($this->kernel);
+        unset($this->session);
         gc_collect_cycles();
         parent::tearDown();
     }
