f0541426ff68df55e5d981b72029f5d7e27eb589
Richard Hülsberg <richard.huelsberg@twt.de>
2024-09-04 15:44:47 +0200

    feat(translation): fixing english translation

--- src/Localization/lang/en/messages.php
+++ src/Localization/lang/en/messages.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'trans' => 'Test System Translation.',
-    'trans.value' => 'Willkommen, {firstname} {lastname}',
-    'trans.choice' => '[0] Null|[1,9] Eins bis Neun|[10,99] Zehn bis Neunundneunzig|[100,999] Eine dreistellige Zahl|[1000, *] Über Tausend!',
-    'trans.choice.value' => '[0] Null {test}|[1,9] Einstellig {test}|[10,99] Zweistellig {test}|[100,999] Dreistellig {test}|[1000, *] mehr als Tausend. {test}',
+    'trans.value' => 'Welcome, {firstname} {lastname}',
+    'trans.choice' => '[0] Null|[1,9] One to nine|[10,99] Ten to ninety-nine|[100,999] A three-digit number|[1000, *] Over a thousand!',
+    'trans.choice.value' => '[0] Null {test}|[1,9] Single digit {test}|[10,99] Two digits {test}|[100,999] Three digits {test}|[1000, *] more than a thousand. {test}',
 ];
